5a3d048525a450777f4035ee68e8a5826354cc19
Johan Abel Taveras <1988jnts@gmail.com>
2025-08-31 21:26:03 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -205,7 +205,7 @@ const renderSongs = (array) => {
 
   if (userData?.songs.length === 0) {
     const resetButton = document.createElement("button");
-    const resetText = document.createTextNode("Reset Playlist")
+  
   
   }
 
